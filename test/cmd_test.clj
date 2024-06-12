c2530a5c7dc93f334d87ffec671d6ecf8413248c
Jude Payne <judepayne@yahoo.co.uk>
2024-06-12 12:16:51 +0100

    tweak native build

--- test/cmd_test.clj
+++ test/cmd_test.clj
@@ -4,24 +4,34 @@
 
 ;; test script to be run in .circleci environment
 ;; assume: bb, bbin & dictim installed
+(require '[babashka.cli :as cli])
+
+(def cli-options {:native? {:default true}})
+
+
+(def native? (:native? (cli/parse-opts *command-line-args* {:spec cli-options})))
+
+
+(def dictim-cmd (if native? "./bin/dictim" "dictim"))
+
 
 (require '[babashka.process :refer [shell]])
 (require '[clojure.edn :refer [read-string]])
 
 ;; test that dictim is installed
-(assert (:out (shell {:out :string} "dictim" "-v")))
+(assert (:out (shell {:out :string} dictim-cmd "-v")))
 
 
 (def dict [:plan "build the resource"])
 (def d2 "plan: build the resource\n")
 
 ;; test compilation
-(assert (= (:out (shell {:out :string :in (pr-str dict)} "dictim" "-c"))
+(assert (= (:out (shell {:out :string :in (pr-str dict)} dictim-cmd "-c"))
            d2))
 
 
 ;; test parsing
-(assert (= (read-string (:out (shell {:out :string :in d2} "dictim" "-k" "-p")))
+(assert (= (read-string (:out (shell {:out :string :in d2} dictim-cmd "-k" "-p")))
            (list dict)))
 
 
@@ -54,7 +64,7 @@ ABC4 -> 1STR: rates trade data
 "[\n  {\n    \"direction\":\"up\"\n  },\n  [\n    \"1STR\",\n    [\n      \"db\",\n      \"sql server\"\n    ],\n    [\n      \"1.5TB\",\n      {\n        \"shape\":\"cylinder\"\n      }\n    ],\n    [\n      \"middle_tier\",\n      \"java\"\n    ],\n    [\n      \"spring boot\",\n      {\n        \"style.border-radius\":8\n      }\n    ],\n    [\n      \"queue\",\n      \"kafka\",\n      {\n        \"shape\":\"queue\"\n      }\n    ],\n    [\n      \"processor\",\n      \"C++ grid\",\n      {\n        \"style.multiple\":true\n      }\n    ],\n    [\n      \"gui\",\n      \"react\"\n    ],\n    [\n      \"gui\"\n    ],\n    [\n      \"db\",\n      \"<->\",\n      \"middle_tier\"\n    ],\n    [\n      \"middle_tier\",\n      \"<->\",\n      \"gui\"\n    ],\n    [\n      \"middle_tier\",\n      \"<->\",\n      \"queue\"\n    ],\n    [\n      \"queue\",\n      \"<->\",\n      \"processor\"\n    ]\n  ],\n  [\n    \"ABC1\",\n    \"->\",\n    \"1STR\",\n    \"client ref data\"\n  ],\n  [\n    \"ABC1\",\n    \"->\",\n    \"1STR\",\n    \"instrument ref data\"\n  ],\n  [\n    \"ABC2\",\n    \"->\",\n    \"1STR\",\n    \"equities trade data\"\n  ],\n  [\n    \"ABC3\",\n    \"->\",\n    \"1STR\",\n    \"fx trades data\"\n  ],\n  [\n    \"ABC4\",\n    \"->\",\n    \"1STR\",\n    \"rates trade data\"\n  ],\n  [\n    \"1STR\",\n    \"->\",\n    \"XYZ1\",\n    \"MIFID reg reports\"\n  ],\n  [\n    \"1STR\",\n    \"->\",\n    \"XYZ2\",\n    \"Other reg reports\"\n  ]\n]\n")
 
 
-(assert (= (:out (shell {:out :string :in d2-2} "dictim" "-j" "-p"))
+(assert (= (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-p"))
            dict-json))
 
 
