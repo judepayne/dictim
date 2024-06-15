7d8796654a92f47c06093d6e2115811af00c3fd9
Jude Payne <judepayne@yahoo.co.uk>
2024-06-15 08:26:31 +0100

    tweak windows native

---
+++ .circleci/script/check-cached.bat
@@ -0,0 +1,14 @@
+"Test presence of large downloads from .circleci caching"
+
+if (Test-Path 'clojure.msi') {
+  Invoke-WebRequest 'https://github.com/casselc/clj-msi/releases/download/v1.11.3.1463/clojure-1.11.3.1463.msi' -OutFile 'clojure.msi'
+}
+
+if (Test-Path -Path 'graalvm') {
+  Invoke-WebRequest 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java17-windows-amd64-22.3.0.zip'  -OutFile 'graalvm.zip'
+  Expand-Archive graalvm.zip graalvm
+}
+
+if (Test-Path 'bb.zip') {
+  Invoke-WebRequest 'https://github.com/borkdude/babashka/releases/download/v1.0.169/babashka-1.0.169-windows-amd64.zip' -OutFile 'bb.zip'
+}
