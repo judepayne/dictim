8a5e30736041d2d6d9ef826e80bc9ae00061d779
Jude Payne <judepayne@yahoo.co.uk>
2024-06-15 11:28:38 +0100

    tweak windows native

--- .circleci/script/check-cached.ps1
+++ .circleci/script/check-cached.ps1
@@ -11,9 +11,13 @@ if (Test-Path -Path 'graalvm') {
   Write-Host 'downloading graalvm..'
   Invoke-WebRequest 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java17-windows-amd64-22.3.0.zip'  -OutFile 'graalvm.zip'
   Expand-Archive graalvm.zip graalvm
+} else {
+  Write-Host 'graalvm available from cache'
 }
 
 if (Test-Path -Path 'bb.zip' -PathType Leaf) {
   Write-Host 'downloading Babashka..'
   Invoke-WebRequest 'https://github.com/borkdude/babashka/releases/download/v1.0.169/babashka-1.0.169-windows-amd64.zip' -OutFile 'bb.zip'
+} else {
+  Write-Host 'Babashka available from cache'
 }
