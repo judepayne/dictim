3f12d89d779d67c420d9d8b2f2976bbee619f5c2
Jude Payne <judepayne@yahoo.co.uk>
2024-06-15 11:39:21 +0100

    tweak windows native

--- .circleci/script/check-cached.ps1
+++ .circleci/script/check-cached.ps1
@@ -1,23 +1,24 @@
 Write-Host 'checking large downloads in cache'
 
 if (Test-Path -Path 'clojure.msi' -PathType Leaf) {
+  Write-Host 'Clojure available from cache'
+} else {
   Write-Host 'downloading Clojure..'
   Invoke-WebRequest 'https://github.com/casselc/clj-msi/releases/download/v1.11.3.1463/clojure-1.11.3.1463.msi' -OutFile 'clojure.msi'
-} else {
-  Write-Host 'Clojure available from cache'
 }
 
 if (Test-Path -Path 'graalvm') {
+  Write-Host 'graalvm available from cache'
+} else {
   Write-Host 'downloading graalvm..'
   Invoke-WebRequest 'https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java17-windows-amd64-22.3.0.zip'  -OutFile 'graalvm.zip'
+  Write-Host 'expanding graalvm archive'
   Expand-Archive graalvm.zip graalvm
-} else {
-  Write-Host 'graalvm available from cache'
 }
 
 if (Test-Path -Path 'bb.zip' -PathType Leaf) {
+  Write-Host 'Babashka available from cache'
+} else {
   Write-Host 'downloading Babashka..'
   Invoke-WebRequest 'https://github.com/borkdude/babashka/releases/download/v1.0.169/babashka-1.0.169-windows-amd64.zip' -OutFile 'bb.zip'
-} else {
-  Write-Host 'Babashka available from cache'
 }
