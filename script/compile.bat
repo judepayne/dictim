3f814e36e7de56cc5144968658f4ffa8ef0209a5
Jude Payne <judepayne@yahoo.co.uk>
2024-06-15 13:47:49 +0100

    tweak win build

--- script/compile.bat
+++ script/compile.bat
@@ -1,4 +1,4 @@
-@echo off
+rem @echo off
 
 if "%GRAALVM_HOME%"=="" (
     echo Please set GRAALVM_HOME
@@ -18,20 +18,20 @@ exit /b
 
 bb uber
 
-call %GRAALVM_HOME%\bin\gu.cmd install native-image
+rem call %GRAALVM_HOME%\bin\gu.cmd install native-image
 
-call %GRAALVM_HOME%\bin\native-image.cmd ^
-  "-cp" "bin\dictim_jvm.jar" ^
-  "-H:Name=bin\dictim.exe" ^
-  "-H:+ReportExceptionStackTraces" ^
-  "--report-unsupported-elements-at-runtime" ^
-  "--verbose" ^
-  "--no-fallback" ^
-  "--no-server" ^
-  "-J-Xmx3g" ^
-  "dictim"
+rem call %GRAALVM_HOME%\bin\native-image.cmd ^
+rem   "-cp" "bin\dictim_jvm.jar" ^
+rem   "-H:Name=bin\dictim.exe" ^
+rem   "-H:+ReportExceptionStackTraces" ^
+rem   "--report-unsupported-elements-at-runtime" ^
+rem   "--verbose" ^
+rem   "--no-fallback" ^
+rem   "--no-server" ^
+rem   "-J-Xmx3g" ^
+rem   "dictim"
 
-if %errorlevel% neq 0 exit /b %errorlevel%
+rem if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo Creating zip archive
-jar -cMf dictim-%VERSION%-windows-amd64.zip bin\dictim.exe
+rem echo Creating zip archive
+rem jar -cMf dictim-%VERSION%-windows-amd64.zip bin\dictim.exe
