417a2cec5756ff1097877100a22490244c5c824d
Jude Payne <judepayne@yahoo.co.uk>
2024-06-17 22:09:23 +0100

    tweak win build

--- script/compile.bat
+++ script/compile.bat
@@ -7,6 +7,7 @@ if "%GRAALVM_HOME%"=="" (
 
 set JAVA_HOME=%GRAALVM_HOME%
 set PATH=%GRAALVM_HOME%\bin;%PATH%
+set PATH="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64_x86";%PATH%
 
 set /P VERSION=< resources\VERSION
 echo Building version %VERSION%
