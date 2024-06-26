e58fb0e2d2022e05a504972e5921e4c0f40449d6
Jude Payne <judepayne@yahoo.co.uk>
2024-06-26 08:01:39 +0100

    fix Win cmd line test & add to appveyor

--- test/cmd_test.clj
+++ test/cmd_test.clj
@@ -45,7 +45,8 @@
     (if (:match? cmp)
       (println cmp)
       (println (str "break at index " (:idx cmp) "\n"
-                    "char 1: " (:char1 cmp) " vs. char2: " (:char2 cmp) "\n"
+                    "char 1: " (:char1 cmp) " (" (int (:char1 cmp))  ") vs. char2: "
+                    (:char2 cmp) " (" (int (:char2 cmp))  ")\n"
                     (subs s1 0 (:idx cmp)))))))
 
 
@@ -60,8 +61,8 @@
 (def d2 (winize "plan: build the resource\n"))
 
 ;; test compilation
-(assert (= (:out (shell {:out :string :in (pr-str dict)} dictim-cmd "-c"))
-           d2))
+(assert (= (str/trim (:out (shell {:out :string :in (pr-str dict)} dictim-cmd "-c")))
+           (str/trim d2)))
 
 
 ;; test parsing
@@ -90,34 +91,26 @@ ABC4 -> 1STR: rates trade data
 1STR -> XYZ2: Other reg reports")
 
 
-(def dict-json (winize "[{\"direction\":\"up\"},[\"1STR\",[\"db\",\"sql server\"],[\"1.5TB\",{\"shape\":\"cylinder\"}],[\"middle_tier\",\"java\"],[\"spring boot\",{\"style.border-radius\":8}],[\"queue\",\"kafka\",{\"shape\":\"queue\"}],[\"processor\",\"C++ grid\",{\"style.multiple\":true}],[\"gui\",\"react\"],[\"gui\"],[\"db\",\"<->\",\"middle_tier\"],[\"middle_tier\",\"<->\",\"gui\"],[\"middle_tier\",\"<->\",\"queue\"],[\"queue\",\"<->\",\"processor\"]],[\"ABC1\",\"->\",\"1STR\",\"client ref data\"],[\"ABC1\",\"->\",\"1STR\",\"instrument ref data\"],[\"ABC2\",\"->\",\"1STR\",\"equities trade data\"],[\"ABC3\",\"->\",\"1STR\",\"fx trades data\"],[\"ABC4\",\"->\",\"1STR\",\"rates trade data\"],[\"1STR\",\"->\",\"XYZ1\",\"MIFID reg reports\"],[\"1STR\",\"->\",\"XYZ2\",\"Other reg reports\"]]\n"))
+(def dict-json "[{\"direction\":\"up\"},[\"1STR\",[\"db\",\"sql server\"],[\"1.5TB\",{\"shape\":\"cylinder\"}],[\"middle_tier\",\"java\"],[\"spring boot\",{\"style.border-radius\":8}],[\"queue\",\"kafka\",{\"shape\":\"queue\"}],[\"processor\",\"C++ grid\",{\"style.multiple\":true}],[\"gui\",\"react\"],[\"gui\"],[\"db\",\"<->\",\"middle_tier\"],[\"middle_tier\",\"<->\",\"gui\"],[\"middle_tier\",\"<->\",\"queue\"],[\"queue\",\"<->\",\"processor\"]],[\"ABC1\",\"->\",\"1STR\",\"client ref data\"],[\"ABC1\",\"->\",\"1STR\",\"instrument ref data\"],[\"ABC2\",\"->\",\"1STR\",\"equities trade data\"],[\"ABC3\",\"->\",\"1STR\",\"fx trades data\"],[\"ABC4\",\"->\",\"1STR\",\"rates trade data\"],[\"1STR\",\"->\",\"XYZ1\",\"MIFID reg reports\"],[\"1STR\",\"->\",\"XYZ2\",\"Other reg reports\"]]\n")
 
 
 
 (def dict-json-pretty
-  (winize "[\n  {\n    \"direction\":\"up\"\n  },\n  [\n    \"1STR\",\n    [\n      \"db\",\n      \"sql server\"\n    ],\n    [\n      \"1.5TB\",\n      {\n        \"shape\":\"cylinder\"\n      }\n    ],\n    [\n      \"middle_tier\",\n      \"java\"\n    ],\n    [\n      \"spring boot\",\n      {\n        \"style.border-radius\":8\n      }\n    ],\n    [\n      \"queue\",\n      \"kafka\",\n      {\n        \"shape\":\"queue\"\n      }\n    ],\n    [\n      \"processor\",\n      \"C++ grid\",\n      {\n        \"style.multiple\":true\n      }\n    ],\n    [\n      \"gui\",\n      \"react\"\n    ],\n    [\n      \"gui\"\n    ],\n    [\n      \"db\",\n      \"<->\",\n      \"middle_tier\"\n    ],\n    [\n      \"middle_tier\",\n      \"<->\",\n      \"gui\"\n    ],\n    [\n      \"middle_tier\",\n      \"<->\",\n      \"queue\"\n    ],\n    [\n      \"queue\",\n      \"<->\",\n      \"processor\"\n    ]\n  ],\n  [\n    \"ABC1\",\n    \"->\",\n    \"1STR\",\n    \"client ref data\"\n  ],\n  [\n    \"ABC1\",\n    \"->\",\n    \"1STR\",\n    \"instrument ref data\"\n  ],\n  [\n    \"ABC2\",\n    \"->\",\n    \"1STR\",\n    \"equities trade data\"\n  ],\n  [\n    \"ABC3\",\n    \"->\",\n    \"1STR\",\n    \"fx trades data\"\n  ],\n  [\n    \"ABC4\",\n    \"->\",\n    \"1STR\",\n    \"rates trade data\"\n  ],\n  [\n    \"1STR\",\n    \"->\",\n    \"XYZ1\",\n    \"MIFID reg reports\"\n  ],\n  [\n    \"1STR\",\n    \"->\",\n    \"XYZ2\",\n    \"Other reg reports\"\n  ]\n]\n"))
+  "[\n  {\n    \"direction\":\"up\"\n  },\n  [\n    \"1STR\",\n    [\n      \"db\",\n      \"sql server\"\n    ],\n    [\n      \"1.5TB\",\n      {\n        \"shape\":\"cylinder\"\n      }\n    ],\n    [\n      \"middle_tier\",\n      \"java\"\n    ],\n    [\n      \"spring boot\",\n      {\n        \"style.border-radius\":8\n      }\n    ],\n    [\n      \"queue\",\n      \"kafka\",\n      {\n        \"shape\":\"queue\"\n      }\n    ],\n    [\n      \"processor\",\n      \"C++ grid\",\n      {\n        \"style.multiple\":true\n      }\n    ],\n    [\n      \"gui\",\n      \"react\"\n    ],\n    [\n      \"gui\"\n    ],\n    [\n      \"db\",\n      \"<->\",\n      \"middle_tier\"\n    ],\n    [\n      \"middle_tier\",\n      \"<->\",\n      \"gui\"\n    ],\n    [\n      \"middle_tier\",\n      \"<->\",\n      \"queue\"\n    ],\n    [\n      \"queue\",\n      \"<->\",\n      \"processor\"\n    ]\n  ],\n  [\n    \"ABC1\",\n    \"->\",\n    \"1STR\",\n    \"client ref data\"\n  ],\n  [\n    \"ABC1\",\n    \"->\",\n    \"1STR\",\n    \"instrument ref data\"\n  ],\n  [\n    \"ABC2\",\n    \"->\",\n    \"1STR\",\n    \"equities trade data\"\n  ],\n  [\n    \"ABC3\",\n    \"->\",\n    \"1STR\",\n    \"fx trades data\"\n  ],\n  [\n    \"ABC4\",\n    \"->\",\n    \"1STR\",\n    \"rates trade data\"\n  ],\n  [\n    \"1STR\",\n    \"->\",\n    \"XYZ1\",\n    \"MIFID reg reports\"\n  ],\n  [\n    \"1STR\",\n    \"->\",\n    \"XYZ2\",\n    \"Other reg reports\"\n  ]\n]\n")
 
 
-(compare-strings (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-p"))
-                 dict-json)
+(assert (= (str/trim (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-p")))
+           (str/trim dict-json)))
 
-(assert (= (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-p"))
-           dict-json))
 
-(compare-strings (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-b" "-p"))
-                 dict-json-pretty)
-
-(assert (= (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-b" "-p"))
-           dict-json-pretty))
+(assert (= (str/trim (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-b" "-p")))
+           (str/trim dict-json-pretty)))
 
 
 
 (def d2
-  (winize "direction: up\n1STR:   {\n  db: sql server\n  1.5TB:  {\n    shape: cylinder\n  }\n  middle_tier: java\n  spring boot:  {\n    style.border-radius: 8\n  }\n  queue: kafka  {\n    shape: queue\n  }\n  processor: C++ grid  {\n    style.multiple: true\n  }\n  gui: react\n  gui\n  db <-> middle_tier\n  middle_tier <-> gui\n  middle_tier <-> queue\n  queue <-> processor\n}\nABC1 -> 1STR: client ref data\nABC1 -> 1STR: instrument ref data\nABC2 -> 1STR: equities trade data\nABC3 -> 1STR: fx trades data\nABC4 -> 1STR: rates trade data\n1STR -> XYZ1: MIFID reg reports\n1STR -> XYZ2: Other reg reports\n"))
+   "direction: up\n1STR:   {\n  db: sql server\n  1.5TB:  {\n    shape: cylinder\n  }\n  middle_tier: java\n  spring boot:  {\n    style.border-radius: 8\n  }\n  queue: kafka  {\n    shape: queue\n  }\n  processor: C++ grid  {\n    style.multiple: true\n  }\n  gui: react\n  gui\n  db <-> middle_tier\n  middle_tier <-> gui\n  middle_tier <-> queue\n  queue <-> processor\n}\nABC1 -> 1STR: client ref data\nABC1 -> 1STR: instrument ref data\nABC2 -> 1STR: equities trade data\nABC3 -> 1STR: fx trades data\nABC4 -> 1STR: rates trade data\n1STR -> XYZ1: MIFID reg reports\n1STR -> XYZ2: Other reg reports\n")
 
 
-(compare-strings (:out (shell {:out :string :in dict-json} dictim-cmd "-c"))
-                 d2)
-
-(assert (= (:out (shell {:out :string :in dict-json} dictim-cmd "-c"))
-           d2))
+(assert (= (str/trim (:out (shell {:out :string :in dict-json} dictim-cmd "-c")))
+           (str/trim d2)))
