baf400235ad7601af2b2d3e48282a0098b8a9bc0
Jude Payne <judepayne@yahoo.co.uk>
2024-06-26 21:56:30 +0100

    tweak cmd_test

--- test/cmd_test.clj
+++ test/cmd_test.clj
@@ -22,32 +22,34 @@
    env))
 
 
-(defn winize [s]
-  (if (= env :win)
-    (str/replace s #"\n" "\r\n")
-    s))
+;; diagnostic function
+#_(defn compare-strings [s1 s2]
+    (let [z (partition 2 (interleave (seq s1) (seq s2)))
+          cmp (reduce
+               (fn [acc [c1 c2]]
+                 (if (= c1 c2)
+                   (update-in acc [:idx] inc)
+                   (reduced (-> acc
+                                (update-in [:idx] inc)
+                                (assoc-in [:match?] false)
+                                (assoc-in [:char1] c1)
+                                (assoc-in [:char2] c2)))))
+               {:idx -1
+                :match? true}
+               z)]
+      (if (:match? cmp)
+        (println cmp)
+        (println (str "break at index " (:idx cmp) "\n"
+                      "char 1: " (:char1 cmp) " (" (int (:char1 cmp))  ") vs. char2: "
+                      (:char2 cmp) " (" (int (:char2 cmp))  ")\n"
+                      (subs s1 0 (:idx cmp)))))))
 
 
-(defn compare-strings [s1 s2]
-  (let [z (partition 2 (interleave (seq s1) (seq s2)))
-        cmp (reduce
-             (fn [acc [c1 c2]]
-               (if (= c1 c2)
-                 (update-in acc [:idx] inc)
-                 (reduced (-> acc
-                              (update-in [:idx] inc)
-                              (assoc-in [:match?] false)
-                              (assoc-in [:char1] c1)
-                              (assoc-in [:char2] c2)))))
-             {:idx -1
-              :match? true}
-             z)]
-    (if (:match? cmp)
-      (println cmp)
-      (println (str "break at index " (:idx cmp) "\n"
-                    "char 1: " (:char1 cmp) " (" (int (:char1 cmp))  ") vs. char2: "
-                    (:char2 cmp) " (" (int (:char2 cmp))  ")\n"
-                    (subs s1 0 (:idx cmp)))))))
+;; we need to compare right trimmed output since the cmd line tool uses
+;; println for clean, human readable output. println adds \n in mac/linux
+;; but \r\n in windows.
+(defn trim= [s1 s2]
+  (= (str/trimr s1) (str/trimr s2)))
 
 
 (require '[babashka.process :refer [shell]])
@@ -58,11 +60,11 @@
 
 
 (def dict [:plan "build the resource"])
-(def d2 (winize "plan: build the resource\n"))
+(def d2 "plan: build the resource\n")
 
 ;; test compilation
-(assert (= (str/trim (:out (shell {:out :string :in (pr-str dict)} dictim-cmd "-c")))
-           (str/trim d2)))
+(assert (trim= (:out (shell {:out :string :in (pr-str dict)} dictim-cmd "-c"))
+               d2))
 
 
 ;; test parsing
@@ -99,18 +101,17 @@ ABC4 -> 1STR: rates trade data
   "[\n  {\n    \"direction\":\"up\"\n  },\n  [\n    \"1STR\",\n    [\n      \"db\",\n      \"sql server\"\n    ],\n    [\n      \"1.5TB\",\n      {\n        \"shape\":\"cylinder\"\n      }\n    ],\n    [\n      \"middle_tier\",\n      \"java\"\n    ],\n    [\n      \"spring boot\",\n      {\n        \"style.border-radius\":8\n      }\n    ],\n    [\n      \"queue\",\n      \"kafka\",\n      {\n        \"shape\":\"queue\"\n      }\n    ],\n    [\n      \"processor\",\n      \"C++ grid\",\n      {\n        \"style.multiple\":true\n      }\n    ],\n    [\n      \"gui\",\n      \"react\"\n    ],\n    [\n      \"gui\"\n    ],\n    [\n      \"db\",\n      \"<->\",\n      \"middle_tier\"\n    ],\n    [\n      \"middle_tier\",\n      \"<->\",\n      \"gui\"\n    ],\n    [\n      \"middle_tier\",\n      \"<->\",\n      \"queue\"\n    ],\n    [\n      \"queue\",\n      \"<->\",\n      \"processor\"\n    ]\n  ],\n  [\n    \"ABC1\",\n    \"->\",\n    \"1STR\",\n    \"client ref data\"\n  ],\n  [\n    \"ABC1\",\n    \"->\",\n    \"1STR\",\n    \"instrument ref data\"\n  ],\n  [\n    \"ABC2\",\n    \"->\",\n    \"1STR\",\n    \"equities trade data\"\n  ],\n  [\n    \"ABC3\",\n    \"->\",\n    \"1STR\",\n    \"fx trades data\"\n  ],\n  [\n    \"ABC4\",\n    \"->\",\n    \"1STR\",\n    \"rates trade data\"\n  ],\n  [\n    \"1STR\",\n    \"->\",\n    \"XYZ1\",\n    \"MIFID reg reports\"\n  ],\n  [\n    \"1STR\",\n    \"->\",\n    \"XYZ2\",\n    \"Other reg reports\"\n  ]\n]\n")
 
 
-(assert (= (str/trim (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-p")))
-           (str/trim dict-json)))
+(assert (trim= (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-p"))
+               dict-json))
 
 
-(assert (= (str/trim (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-b" "-p")))
-           (str/trim dict-json-pretty)))
-
+(assert (trim= (:out (shell {:out :string :in d2-2} dictim-cmd "-j" "-b" "-p"))
+               dict-json-pretty))
 
 
 (def d2
    "direction: up\n1STR:   {\n  db: sql server\n  1.5TB:  {\n    shape: cylinder\n  }\n  middle_tier: java\n  spring boot:  {\n    style.border-radius: 8\n  }\n  queue: kafka  {\n    shape: queue\n  }\n  processor: C++ grid  {\n    style.multiple: true\n  }\n  gui: react\n  gui\n  db <-> middle_tier\n  middle_tier <-> gui\n  middle_tier <-> queue\n  queue <-> processor\n}\nABC1 -> 1STR: client ref data\nABC1 -> 1STR: instrument ref data\nABC2 -> 1STR: equities trade data\nABC3 -> 1STR: fx trades data\nABC4 -> 1STR: rates trade data\n1STR -> XYZ1: MIFID reg reports\n1STR -> XYZ2: Other reg reports\n")
 
 
-(assert (= (str/trim (:out (shell {:out :string :in dict-json} dictim-cmd "-c")))
-           (str/trim d2)))
+(assert (trim= (:out (shell {:out :string :in dict-json} dictim-cmd "-c"))
+               d2))
