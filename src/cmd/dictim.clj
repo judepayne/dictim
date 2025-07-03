38e132950173172b32891d0100ec518f3dcb74dc
Jude Payne <judepayne@yahoo.co.uk>
2025-07-03 12:01:59 +0100

    tweak command line tool help

--- src/cmd/dictim.clj
+++ src/cmd/dictim.clj
@@ -137,7 +137,7 @@
     :graph {:desc graph-help
             :alias :g}
     :version {:coerce :boolean
-              :desc "Returns the version of dictim."
+              :desc "Returns the version of this command line tool."
               :alias :v}
     :help {:coerce :boolean
            :desc "Displays this help."
